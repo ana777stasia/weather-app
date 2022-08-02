3bf3fc380ce46a4f82196af8fd0b9f4dbe24e23f
ana777stasia <ppc1312@gmail.com>
2022-08-02 17:15:19 +0200

    remove console.log

--- script.js
+++ script.js
@@ -74,7 +74,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "f26f7a1bde9f9ef7818f1bda2d4d548a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(displayForecast);
